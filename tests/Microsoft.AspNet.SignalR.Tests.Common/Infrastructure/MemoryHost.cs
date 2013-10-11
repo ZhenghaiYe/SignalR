1819b1ddaec89d37a48ae774603277a8d560b3fe
Gustavo Armenta <gustavo.armenta@gmail.com>
2013-10-11 16:42:41 -0700

    replace MemoryHost with TestServer

---
+++ tests/Microsoft.AspNet.SignalR.Tests.Common/Infrastructure/MemoryHost.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client.Http;
+using Microsoft.AspNet.SignalR.Infrastructure;
+using Microsoft.Owin.Hosting;
+using Microsoft.Owin.Testing;
+using Owin;
+
+namespace Microsoft.AspNet.SignalR.Hosting.Memory
+{
+    public class MemoryHost : DefaultHttpClient, IDisposable
+    {
+        private static int instanceId;
+        private TestServer _host;
+
+        public string InstanceName { get; set; }        
+
+        public MemoryHost()
+        {
+            var id = Interlocked.Increment(ref instanceId);
+            InstanceName = Process.GetCurrentProcess().ProcessName + id;
+        }
+
+        public void Configure(Action<IAppBuilder> startup)
+        {
+            _host = TestServer.Create(startup);
+        }
+
+
+        protected override HttpMessageHandler CreateHandler()
+        {
+            return _host.Handler;
+        }
+
+        public void Dispose()
+        {
+            _host.Close();
+        }        
+    }
+}
